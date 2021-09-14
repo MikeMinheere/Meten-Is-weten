35e618ffa409898d557058742eff30a08761939b
Mike Minheere <99067548@mydavinci.nl>
2021-09-14 09:39:12 +0200

    elif-statement

--- meten-is-weten.py
+++ meten-is-weten.py
@@ -1,6 +1,9 @@
 A = input('kies een heel getal tussen de 1 en de 100: ')
 B = input('Kies nog een heel getal tussen de 1 en de 100: ')
 MAX = A
-if (A >= B):
+MIN = A
+if (A > B):
     print('A is het grootste getal: ' + str(MAX))
+elif (A < B):
+    print('A is het kleinste getal: ' + str(MIN))
 else :print('B is groter dan A')
