35e3413e8b25aa37c6502351aa46951b9c607a43
Mike Minheere <99067548@mydavinci.nl>
2021-09-14 09:35:06 +0200

    imput en statement

---
+++ meten-is-weten.py
@@ -0,0 +1,6 @@
+A = input('kies een heel getal tussen de 1 en de 100: ')
+B = input('Kies nog een heel getal tussen de 1 en de 100: ')
+MAX = A
+if (A >= B):
+    print('A is het grootste getal: ' + str(MAX))
+else :print('B is groter dan A')
