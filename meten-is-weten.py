42916138939cc90414e8b27c4e62d87d918dfa0a
Mike Minheere <99067548@mydavinci.nl>
2021-09-14 09:49:30 +0200

    Min en Max

--- meten-is-weten.py
+++ meten-is-weten.py
@@ -1,9 +1,13 @@
-A = input('kies een heel getal tussen de 1 en de 100: ')
-B = input('Kies nog een heel getal tussen de 1 en de 100: ')
-MAX = A
-MIN = A
+A = input('kies een heel getal: ')
+B = input('Kies nog een heel getal: ')
+MAX = int(A)
+MIN = int(A)
 if (A > B):
     print('A is het grootste getal: ' + str(MAX))
+    MIN = B
 elif (A < B):
     print('A is het kleinste getal: ' + str(MIN))
-else :print('B is groter dan A')
+    MAX = B
+else :print("A is evengroot als B")
+print("met minimum is: "+ str(MIN))
+print("het maximum is: "+ str(MAX))
